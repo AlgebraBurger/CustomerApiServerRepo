ae9ed572d5725e02b746bfbc4f6d2ca3169361ee
juliusbacosa <juliusbacosa@gmail.com>
2015-11-05 11:49:32 +0800

    Initial Commit

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerApiServer.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class CrmDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
